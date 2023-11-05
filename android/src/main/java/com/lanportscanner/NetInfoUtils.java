895fa53515ce5ecc93cd09e3ccffcfb3c98a3e04
Tejas Gajjar <gajjartejas26@gmail.com>
2023-11-05 19:54:29 +0530

    feat: library update

--- android/src/main/java/com/lanportscanner/NetInfoUtils.java
+++ android/src/main/java/com/lanportscanner/NetInfoUtils.java
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package com.reactnativelanportscanner;
+package com.lanportscanner;
 
 import android.Manifest;
 import android.content.Context;
